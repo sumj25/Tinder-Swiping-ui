3612541dd902454ab518dc42783328b678a7f2b7
sumj25 <sumanj252001@gmail.com>
2020-12-11 18:47:30 +0530

    Adding Files

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  Animated,
+  PanResponder,
+Dimensions,
+  StyleSheet,
+  Image,Text
+} from 'react-native';
+import { Card, Paragraph } from 'react-native-paper';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Tinder from './components/Tinder';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH= Dimensions.get('window').width
+const mydata = [
+  {
+    id: 1,
+    text: 'card 1',
+    uri: 'https://th.bing.com/th/id/OIP.tUdxadzYOcnOuOEyCPc9uQHaKt?pid=Api&rs=1'
   },
-});
+  { id: 2, text: 'card 2', uri: 'https://th.bing.com/th/id/OIP.sZR8glsYEOuhC4Wbxu2K0AHaJ4?pid=Api&rs=1' },
+  {
+    id: 3,
+    text: 'card 3',
+    uri: 'https://th.bing.com/th/id/OIP.6nStQOiedolM-F72C8FbrQHaIS?pid=Api&rs=1'
+  },
+  { id: 4, text: 'card 4', uri: 'https://th.bing.com/th/id/OIP.b2EUJ1Gcj7f2bMjB28D_7gHaJ4?pid=Api&rs=1' },
+  { id: 5, text: 'card 5', uri: 'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2019/04/03/808708-773760-deepika-padukone-10.jpg' },
+  { id: 6, text: 'card 6', uri: 'https://source.unsplash.com/RfoISVdKM4U' },
+  { id: 7, text: 'card 6', uri: 'https://th.bing.com/th/id/OIP.ReLgSprWUzJ5PNQz5_aNQAHaLO?pid=Api&rs=1' },
+  
+];
+
+class App extends React.Component {
+  
+  renderCard(item){
+    return(
+      <View style={{margin:10}} key={item.id}>
+      <View style={{height:60}}>
+      
+      </View>
+     
+      <Image
+      style={{flex:1,height:SCREEN_HEIGHT-120,width:null,resizeMode:'cover',borderRadius: 40}}
+       source={{uri:item.uri}}
+       />
+      
+     
+     </View>
+    )
+  }
+  renderNoMoreCards(){
+    return(
+    <View style={{margin:50}}>
+    <Card>
+    <Card.Title title="sab thik hai" />
+    <Paragraph>sab kuchh chalta haii</Paragraph></Card>
+    <Text>"alone"</Text>
+  
+    </View>)
+  }
+  render(){
+    return(
+      <View >
+     
+      <Tinder
+      data={mydata}
+      renderCards={this.renderCard}
+      renderNoMoreCards={this.renderNoMoreCards} />
+     
+      </View>
+    
+    )
+  }}
+  const styles=StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor:'#fff',
+      margin:10
+     
+    }
+  })
+
+export default App;
---
+++ components/Tinder.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Animated,
+  PanResponder,
+  Dimensions,
+  LayoutAnimation,
+  UIManager,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+import { LogBox } from 'react-native';
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const SWIPE_LIMIT =SCREEN_WIDTH /2;
+class Tinder extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            index:0
+        }
+        const position = new Animated.ValueXY();
+        this.panResponder = PanResponder.create({
+        onStartShouldSetPanResponder:()=> true,
+        onPanResponderMove:(e,gesture)=>{
+            position.setValue({x:gesture.dx,y:gesture.dy})
+        },
+        onPanResponderRelease:(e,gesture)=>{
+            if(gesture.dx>SWIPE_LIMIT){
+            this.swiped("right")
+            }
+            if(gesture.dx <-SWIPE_LIMIT){
+           this.swiped("left")
+            }
+            else{
+            this.reserPosition()}
+        }
+        })
+        
+        this.position=position
+        this.likeOpacity=position.x.interpolate({
+            inputRange:[-SCREEN_WIDTH,0,SCREEN_WIDTH],
+            outputRange:[0,0,8],
+            extrapole:'clamp'
+          })
+          this.dislikeOpacity=position.x.interpolate({
+            inputRange:[-SCREEN_WIDTH,0,SCREEN_WIDTH],
+            outputRange:[8,0,0],
+            extrapole:'clamp'
+          })
+    }
+    componentDidUpdate(nextProps) {
+        LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
+        UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+        LayoutAnimation.spring();}
+    
+    swiped(direction){
+        const x= direction==='right' ?SCREEN_WIDTH*3: -SCREEN_WIDTH*3
+        Animated.timing(this.position,{
+            toValue:{x:x,y:0},
+           
+        }).start(()=>{
+            this.position.setValue({x:0,y:0}),
+            this.setState({index:this.state.index+1})
+        })
+    }
+    reserPosition(){
+        Animated.spring(this.position,{
+            toValue:{x:0,y:0},
+         
+            
+        }).start()
+    }
+    mycardStyle(){
+        const rotate =this.position.x.interpolate({
+            inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH/2],
+            outputRange:['-10deg','0deg','10deg'],
+            extrapolate:'clamp'
+        })
+return {
+   ...this.position.getLayout(),
+   transform:[{rotate:rotate}]
+}
+    }
+    rendercard() {
+        if(this.state.index >= this.props.data.length){
+            return this.props.renderNoMoreCards()
+        }
+        return this.props.data.map((item,i)=>{
+            if(i<this.state.index){
+                return null
+            }
+            if(i===this.state.index){
+                return(
+                    <Animated.View 
+                    key={item.id} style={[this.mycardStyle(),styles.cardStyle]} 
+                    {...this.panResponder.panHandlers}>
+                    
+                    <Animated.View style={{opacity:this.likeOpacity,transform:[{rotate:'30deg'}],position:'absolute',top:90,right:50,zIndex:1000}}>
+      <Text style={{borderWidth:1,borderColor:'red',color:'red',fontSize:30,fontWeight:'900',padding:10}}>NOPE</Text></Animated.View>
+      <Animated.View style={{opacity:this.dislikeOpacity,transform:[{rotate:'-30deg'}],position:'absolute',top:90,left:50,zIndex:1000}}>
+      <Text style={{borderWidth:1,borderColor:'green',color:'green',fontSize:32,fontWeight:'900',padding:10}}>LIKE</Text></Animated.View>
+                   {this.props.renderCards(item)}
+                    </Animated.View >
+                )
+            }
+            return (
+                <Animated.View key={item.id} style={[styles.cardStyle]}>
+                {this.props.renderCards(item)} 
+                </Animated.View>
+            )
+            
+        }).reverse()
+    }
+    render(){
+        return(
+            <View>
+            <View style={{flex: 1}}>
+           
+            <View style={{flex: 1}}></View>
+             {this.rendercard()}
+             </View>
+             <View style={{height:60}}>
+
+             
+  
+             </View>
+             </View>
+           
+        )
+    }
+}
+const styles = StyleSheet.create({
+    cardStyle:{
+        position:"absolute",
+        zIndex:1,
+        width:SCREEN_WIDTH
+    }
+})
+export default Tinder;
